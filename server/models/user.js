dce1eecc0d532b1b4af1ab5618a15021094e68ea
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-15 12:04:44 +0530

    ismodified

--- server/models/user.js
+++ server/models/user.js
@@ -41,23 +41,22 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save',function(next){
     var user = this;
-
-    bcrypt.genSalt(10,function(err,salt){
-        if (err){
-            return next(err)
-        }
-        bcrypt.hash(user.password,salt,function(err,hash){
+    if(user.isModified(user.password)){
+        bcrypt.genSalt(10,function(err,salt){
             if (err){
                 return next(err)
             }
-            else{
-                user.password = hash;
-                next();
-            }
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if (err){
+                    return next(err)
+                }
+                else{
+                    user.password = hash;
+                    next();
+                }
+            })
         })
-    })
-
-    
+    }    
 })
 
 const User = mongoose.model('User_m', userSchema);
