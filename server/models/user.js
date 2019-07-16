55851727b285d06456cbb46b85f8275c25511160
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-16 22:58:21 +0530

    auth given

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,16 @@ userSchema.methods.generateToken = function(cb){
         if(err) return cb(err);
         cb(null,user)
     })
+};
+
+userSchema.statics.findByToken = function(token,cb){
+    var user = this;
+    jwt.verify(token,process.env.SECRET,function(err,decode){
+        user.findOne({"_id":decode, "token":token},function(err,user){
+            if(err)return cb(err)
+            cb(null,user)
+        })
+    })
 }
 
 const User = mongoose.model('User_m', userSchema);
